cd8f3323867a18c8041385314e778531a1a226ce
Klaudia Szczepańska <87570436+KSzczepa@users.noreply.github.com>
2026-01-13 21:52:42 +0100

    feat: enhance user component to emit selected user ID and update app template for user data binding

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,14 +7,17 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.css',
 })
 
-export class UserComponent {
+export class UserComponent {  
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath(): string {
     return `assets/users/${this.avatar}`;
   }
 
   onSelectedUser() {
+    this.select.emit(this.id);
   }
 }
