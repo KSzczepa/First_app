270e6f104228ea232d9653a51450c145019e71fb
Klaudia Szczepańska <87570436+KSzczepa@users.noreply.github.com>
2026-02-04 21:11:53 +0100

    feat: implement task management service and refactor task handling in components

---
+++ src/app/tasks/task/tasks.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { Task } from "./task";
+import { TaskData, FormData } from "./task.model";
+
+@Injectable({ providedIn: 'root' })
+export class TasksService {
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the core concepts of Angular framework and important features.',
+      dueDate: '2026-12-12',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build an App',
+      summary: 'Apply Angular knowledge by building a real-world application from scratch.',
+      dueDate: '2026-11-30',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Test and Deploy',
+      summary: 'Learn how to test Angular applications and deploy them to production environments.',
+      dueDate: '2027-01-15',
+    },
+  ];
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter(task => task.userId === userId);
+  }
+
+  addTask(formData: FormData, userId: string) {
+    this.tasks.unshift({
+      id: 't' + (this.tasks.length + 1),
+      userId: userId,
+      title: formData.title,
+      summary: formData.summary,
+      dueDate: formData.dueDate
+    });
+  }
+
+  removeTask(taskId: string) {
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+}
--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -2,7 +2,8 @@ import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 import { Task } from './task/task';
 import { NewTaskTemplate } from './new-task-template/new-task-template';
 import { FormData } from './task/task.model';
-import { Card } from "../shared/card/card";
+import { Card } from '../shared/card/card';
+import { TasksService } from './task/tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -10,48 +11,26 @@ import { Card } from "../shared/card/card";
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
-
-export class TasksComponent {  
-  @Input({required: true}) userId!: string;
-  @Input({required: true}) name!: string;
+export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
   @Output() task!: {
     id: string;
     userId: string;
     title: string;
     summary: string;
-    dueDate: string; 
+    dueDate: string;
   };
   showNewTaskForm = false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all the core concepts of Angular framework and important features.',
-      dueDate: '2026-12-12'
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build an App',
-      summary: 'Apply Angular knowledge by building a real-world application from scratch.',
-      dueDate: '2026-11-30'
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Test and Deploy',
-      summary: 'Learn how to test Angular applications and deploy them to production environments.',
-      dueDate: '2027-01-15'
-    }
-  ];
+
+  constructor(private taskService: TasksService) {}
 
   get selectedUserTask() {
-    return this.tasks.filter(task => task.userId === this.userId);
-  } 
+    return this.taskService.getUserTasks(this.userId);
+  }
 
   onCompleteTask(taskId: string) {
-    this.tasks = this.tasks.filter(task => task.id !== taskId);
+    this.taskService.removeTask(taskId);
   }
 
   onStartAddTask() {
@@ -62,16 +41,8 @@ export class TasksComponent {
     this.showNewTaskForm = false;
   }
 
-  onAddTask(formData: FormData) {
-    this.tasks.push({
-      id: 't' + (this.tasks.length + 1),
-      userId: this.userId,
-      title: formData.title,
-      summary: formData.summary,
-      dueDate: formData.dueDate
-    });
-    console.log(this.tasks);
+  onAddTask(formData: FormData) {    
+    this.taskService.addTask(formData, this.userId);
     this.showNewTaskForm = false;
   }
 }
-
